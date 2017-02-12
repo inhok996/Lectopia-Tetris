a08b1b48db43b7b1bffd11535417dc4ca71c0783
Inho-PC\Inho <inho211@naver.com>
2017-02-12 17:59:27 +0900

    pasteBoard 수정, move시리즈 함수,rotate 정의 추가

--- Tetris.cpp
+++ Tetris.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <malloc.h>
+#include <stdlib.h>
 
 int block[][4][4][4]=
 { //블럭 4차원 배열 첫번쨰배열인덱스: 블럭의 종류 두번째배열인덱스 :블럭의 회전
@@ -17,20 +18,21 @@ int block[][4][4][4]=
 			{0,0,1,0}, 
 			{0,1,1,0}, 
 			{0,0,0,0}, 
-			},
-			{
-				{1,1,1,0}, 
-				{0,0,1,0}, 
-				{0,0,0,0}, 
-				{0,0,0,0}, 
-			},
-			{
-				{1,1,0,0}, 
-				{1,0,0,0}, 
-				{1,0,0,0}, 
-				{0,0,0,0}, 
-				}
+		},
+		{
+			{1,1,1,0}, 
+			{0,0,1,0}, 
+			{0,0,0,0}, 
+			{0,0,0,0}, 
+		},
+		{
+			{1,1,0,0}, 
+			{1,0,0,0}, 
+			{1,0,0,0}, 
+			{0,0,0,0}, 
+		}
 	}
+	
 };
 
 
@@ -70,11 +72,11 @@ void pasteBlock(int** bo, int(*bl)[4], int x, int y)
 //설명 : 블럭이 붙여진 보드를 원래 배경으로 되돌려 놓는다.
 //Postcondition : None
 //return : None
-void pasteBoard(int**bPb, int** bo,int x,int y)
+void pasteBoard(int**bPb, int** bo)
 {
-	for(int i = 0 ; i < BLOCK_HEIGHT; i++){
-		for(int j = 0 ; j < BLOCK_WIDTH ; j++){
-			bPb[i+y][j+x] = bo[i+y][j+x];
+	for(int i = 0 ; i < BOARD_HEIGHT + WALLSIZE ; i++){
+		for(int j = 0 ; j < BOARD_WIDTH + (WALLSIZE * 2) ; j++){
+			bPb[i][j] = bo[i][j];
 		}
 	}
 }
@@ -90,9 +92,9 @@ void initGame(Tetris* te)
 	int i, j;
 	te->x = (BOARD_WIDTH + WALLSIZE * 2) / 2 - 2;
 	te->y = 0;
-	te->board = (char **)calloc(1, sizeof(char *)*(BOARD_HEIGHT + WALLSIZE));
+	te->board = (int **)calloc(1, sizeof(int *)*(BOARD_HEIGHT + WALLSIZE));
 	assert(te->board != NULL);
-	te->boPlusbl = (char **)calloc(1, sizeof(char *)*(BOARD_HEIGHT + WALLSIZE));
+	te->boPlusbl = (int **)calloc(1, sizeof(int *)*(BOARD_HEIGHT + WALLSIZE));
 	assert(te->boPlusbl != NULL);
 	te->gameState = GAME_OVER;
 	te->blockState = 0; //회전 4가지모양 
@@ -101,8 +103,8 @@ void initGame(Tetris* te)
 
 	for (i = 0; i < BOARD_HEIGHT + WALLSIZE; i++)
 	{
-		te->board[i] = (char *)calloc(1, sizeof(char)*(BOARD_WIDTH + (WALLSIZE * 2)));
-		te->boPlusbl[i]= (char *)calloc(1, sizeof(char)*(BOARD_WIDTH + (WALLSIZE * 2)));
+		te->board[i] = (int *)calloc(1, sizeof(int)*(BOARD_WIDTH + (WALLSIZE * 2)));
+		te->boPlusbl[i]= (int *)calloc(1, sizeof(int)*(BOARD_WIDTH + (WALLSIZE * 2)));
 	}
 
 	for (i = 0; i < BOARD_HEIGHT + WALLSIZE; i++)
@@ -122,7 +124,19 @@ void initGame(Tetris* te)
 //설명 : bpb의 블럭이 붙여진 부분을 체크하여 충돌이 일어났는지를 체크한다.(값이 2인지를 확인)
 //Postcondition : None
 //return : 1 (crash) , 0 (not crash)
-int crashCheck(int** bpb, int x, int y){return 1;}
+int crashCheck(int** bpb, int x, int y)
+{
+	int i, j;
+	for (i = 0; i < BLOCK_HEIGHT; i++)
+	{
+		for (j = 0; j < BLOCK_HEIGHT; j++)
+		{
+			if (bpb[y + i][x+ j] == 2)
+				return true;
+		}
+	}
+	return false;
+}
 
 //함수명 : lineCheck
 //Precondition : bpb , y는 적절한 값으로 초기화되어 있다.
@@ -143,26 +157,109 @@ int lineErase(int** bpb, int y){return 1;}
 //설명 : 게임을 초기화시키고 동적할당한 메모리를 모두 해제시킨다. Score를 리턴한다.
 //Postcondition : board 및 boPlusbl의 메모리 해제
 //return : None
-void gameOver(Tetris* Te){}
+void gameOver(Tetris* te){}
 
 //함수명 : createBlock 
-//Precondition : Tetris 구조체의 값이 적절히 차 있다. 
+//Precondition : Tetris 구조체의 값이 적절히 차 있다., srand가 1회 이상 호출되어야 한다.
 //설명 : 랜덤으로 블럭을 생성한다. 
 //Postcondition : Te->whichBlock = rand();, blockState = 0 으로 초기화된다. 
 //return : None 
-void createBlock(Tetris* Te){}
+void createBlock(Tetris* te)
+{
+	te->x = (BOARD_WIDTH + WALLSIZE * 2) / 2 - 2; //x,y 초기화
+	te->y = 0; 
+
+	//te->whichBlock = rand() % 7; //whichBlock은 0~6사이의 값
+	te->whichBlock = 0;
+	te->blockState = 0; //회전상태를 초기값으로
+}
+
 
 //함수명 : moveLeft
 //return : 1(성공), 0 실패
 int moveLeft(Tetris* te)
-{}
+{
+	pasteBoard(te->boPlusbl,te->board); //일단 움직이기 전에 bpb를 board로 초기화
+	te->x--; //좌표이동
+	pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+	if(crashCheck(te->boPlusbl,te->x,te->y)){ //만약 충돌하면
+		pasteBoard(te->boPlusbl,te->board); //bpb 원위치
+		te->x++;//좌표를 돌려놓음
+		pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+		return 0;//실패
+	}
+	return 1; //성공
+}
 //함수명 : move Right
 //return : 1(성공) 0 실패
 int moveRight(Tetris* te)
-{}
+{
+	pasteBoard(te->boPlusbl,te->board); //일단 움직이기 전에 bpb를 board로 초기화
+	te->x++; //좌표이동
+	pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+	if(crashCheck(te->boPlusbl,te->x,te->y)){ //만약 충돌하면
+		pasteBoard(te->boPlusbl,te->board); //bpb 원위치
+		te->x--;//좌표를 돌려놓음
+		pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+		return 0;//실패
+	}
+	return 1; //성공
+}
 //함수명 : move Down
 //return : 1(성공) 0 실패
 int moveDown(Tetris* te)
-{}
+{
+	pasteBoard(te->boPlusbl,te->board); //일단 움직이기 전에 bpb를 board로 초기화
+	te->y++; //좌표이동
+	pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+	if(crashCheck(te->boPlusbl,te->x,te->y)){ //만약 충돌하면
+		pasteBoard(te->boPlusbl,te->board); //bpb 원위치
+		te->y--;//좌표를 돌려놓음
+		pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+
+		//바닥에 도달 -> board(배경)을 bpb으로 초기화
+		pasteBoard(te->board,te->boPlusbl);
+		createBlock(te);
+		pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y);
+		//full line 확인
+		//game over 확인
+
+		return 0;//실패
+	}
+	return 1; //성공
+
+}
 int rotate(Tetris* te)
-{}
+{
+	int pre_blockstate = te->blockState;//이 전의 회전값을 저장
+	pasteBoard(te->boPlusbl,te->board); //일단 움직이기 전에 bpb를 board로 초기화
+	te->blockState = (te->blockState + 1) % 4;
+	pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+	if(crashCheck(te->boPlusbl,te->x,te->y)){ //만약 충돌하면
+		pasteBoard(te->boPlusbl,te->board); //bpb 원위치
+		te->blockState = pre_blockstate;
+		pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+		return 0;//실패
+	}
+	return 1; //성공
+
+}
+
+int spaceMove(Tetris* te)
+{
+	while(!crashCheck(te->boPlusbl,te->x,te->y)){ //충돌할때까지 체크
+		pasteBoard(te->boPlusbl,te->board); //움직이기 전 bPb를 배경으로/
+		te->y++;//좌표 이동
+		pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y);//붙여넣음
+	}
+	//충돌 후에
+	pasteBoard(te->boPlusbl,te->board); //bpb 원위치
+	te->y--;//좌표를 돌려놓음
+	pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y); //block 붙이기
+
+	//바닥에 도달 -> board(배경)을 bpb으로 초기화
+	pasteBoard(te->board,te->boPlusbl);
+	createBlock(te);
+	pasteBlock(te->boPlusbl,block[te->whichBlock][te->blockState],te->x,te->y);
+	return 1; //성공
+}
